f974c3eadff702391fb1e74aedc57749cdfe2186
MichaelShem <89414581+MichaelShem@users.noreply.github.com>
2022-02-25 19:33:25 +0300

    Update test_items.py
    Добавлены файлы для рецензирования

--- test_items.py
+++ test_items.py
@@ -6,7 +6,7 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_find_button_add_card(browser):
     browser.get(link)
-    assert browser.find_elements(By.CSS_SELECTOR, '.product_main > button'), "Кнопка не найдена"
+    assert browser.find_elements(By.CLASS_NAME, 'btn-add-to-basket'), "Кнопка не найдена"
     time.sleep(30)
 
 """
