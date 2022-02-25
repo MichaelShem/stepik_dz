f01838b3f7852bc20bd188de141b1218e2e11322
Michael_Shem <Minya2044@yandex.ru>
2022-02-25 18:50:13 +0300

    Добавление файлов на рецензирование

---
+++ lesson6_step9_multi_language_dz.py
@@ -0,0 +1,15 @@
+from selenium.webdriver.common.by import By
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+
+
+def test_guest_should_see_login_link(browser):
+    browser.get(link)
+    assert browser.find_elements(By.CSS_SELECTOR, '.product_main > button'), "Кнопка не найдена"
+    #time.sleep(30)
+
+"""
+Запускаем тест
+    pytest --language=es lesson6_step9_multi_language_dz.py
+"""
+
